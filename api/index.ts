fd8c62a77c0b8c0105ceb020fb8e17c5759fa233
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-20 16:03:21 +0900

    test

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
-const express = require("express");
-const app = express();
+import express from "express";
 
+app = express();
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
+app.get("/hsol", (req, res) => res.send("한솔"));
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+export default app;
