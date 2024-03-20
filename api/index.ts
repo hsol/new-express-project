5dc153209840f78324eb96b79b8ac9f6ee1fecaf
Hansol-Lim <dev.hansollim@gmail.com>
2024-03-20 16:05:54 +0900

    esnext

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import express = require("express");
+import express from "express";
 
 const app = express();
 
@@ -6,4 +6,4 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-module.exports = app;
+export app;
